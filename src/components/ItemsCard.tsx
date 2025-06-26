a65f27154a3d91a1c8fbca15da04763a066c2fdc
OlgaSperrow <osperling17@gmail.com>
2025-06-26 17:56:28 +0200

    add link to item

--- src/components/ItemsCard.tsx
+++ src/components/ItemsCard.tsx
@@ -1,21 +1,33 @@
-
+import { Link } from "react-router";
 import type { Item } from "../common/Item.ts";
+import type Container from "../common/Types.ts";
+import {useEffect, useState} from "react";
+
 
 
 interface Props {
     item: Item;
-    containerColor: string;
+
 }
 
-const ItemsCard = ({ item, containerColor  }: Props) => {
-    return (
-        <li
-            className="p-2 rounded text-black text-center mb-2"
-            style = {{backgroundColor: containerColor}}
+const ItemsCard = ({ item  }: Props) => {
+    const [container, setContainer] = useState<Container | null>(null);
 
+    useEffect(() => {
+        if (item.containerId) {
+            fetch(`/api/containers/${item.containerId}`)
+                .then((res) => res.json())
+                .then(setContainer)
+                .catch(console.error);
+        }
+    }, [item.containerId]);
+    return (
+        <li key={item.id} className="bg-amber-200 rounded-2xl p-2 m-2"
+            style={{ backgroundColor: container?.color || "#f5f5f5" }}
         >
-            <p className="text-black">Item name : {item.name}</p>
-            <p className="text-black">Container ID: {item.containerId}</p>
+            Name: {item.name} Container Id: {item.containerId}
+
+            <Link to={`/items/${item.id}`}>To item page</Link>
         </li>
     );
 };
--- src/pages/ItemDetail.tsx
+++ src/pages/ItemDetail.tsx
@@ -7,63 +7,38 @@ import type Container from "../common/Types.ts";
 
 const ItemDetail = () => {
     const { id } = useParams<{ id: string }>();
-    const [item, setItem] = useState<Item | null>(null);
-    const [container, setContainer] = useState<Container | null>(null);
-    const [message, setMessage] = useState<string | null>(null);
+    const [item, setItem] = useState<Item | undefined>(undefined);
+    const [container, setContainer] = useState<Container | undefined>(undefined);
 
-    async function fetchItem() {
-        try {
+    async function fetchItem(id: string | undefined) {
             const res = await fetch(`/api/items/${id}`);
+            if(!res.ok) {
+                throw new Error("Error loading item details.");
+            }
             const data = await res.json();
             setItem(data);
-        } catch (err) {
-            console.error(err);
-            setMessage("Error loading item details.");
-        }
+            fetchContainer(data.containerId)
     }
 
     async function fetchContainer(containerId: string) {
-        try {
+
             const res = await fetch(`/api/containers/${containerId}`);
+            if(!res.ok) {
+                throw new Error("Error loading container.");
+            }
             const data = await res.json();
             setContainer(data);
-        } catch (err) {
-            console.error(err);
-            setMessage("Error loading container details.");
-        }
+
     }
 
     useEffect(() => {
-        if (id) {
-            fetchItem();
-        }
+            fetchItem(id);
     }, [id]);
 
-    useEffect(() => {
-        if (item?.containerId) {
-            fetchContainer(item.containerId);
-        }
-    }, [item]);
-
-    if (!item) {
-        return <div>Loading item details not jet ready</div>;
-    }
     return (
         <div>
-            <h2 className="text-2xl font-bold mb-4">Item Details</h2>
-            {message ? <p className="text-red-500">{message}</p> : null}
-            <p><strong>ID:</strong> {item.id}</p>
-            <p><strong>Name:</strong> {item.name}</p>
-            {container ? (
-                <div className="mt-4 p-4 border rounded" style={{ backgroundColor: container.color }}>
-                    <h3 className="font-semibold">Container Info:</h3>
-                    <p><strong>Name:</strong> {container.name}</p>
-                    <p><strong>Description:</strong> {container.description}</p>
-                </div>
-            ) : (
-                <p>Loading container info</p>
-            )}
-
+          Item: {item?.name} <span style ={{backgroundColor: container?.color}}>
+            Container: {container?.id}</span>
 
         </div>
     );
